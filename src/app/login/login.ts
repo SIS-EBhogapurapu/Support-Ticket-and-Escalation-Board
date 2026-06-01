39d3ec1b48e99c937ba976e6709395633701b2b3
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-01 14:24:02 +0530

    Login button-working

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone:true,
+  standalone: true,
   templateUrl: './login.html',
   styleUrls: ['./login.css']
 })
 export class LoginComponent {
 
   validateLogin() {
-    const username = (document.getElementById("username") as HTMLInputElement).value.trim().toUpperCase();
-    const password = (document.getElementById("password") as HTMLInputElement).value.trim().toUpperCase();
+    const username = (document.getElementById("Username") as HTMLInputElement).value.trim().toUpperCase();
+    const password = (document.getElementById("Password") as HTMLInputElement).value.trim().toUpperCase();
 
     if ((username === "ADMIN" && password === "ADMIN") ||
         (username === "USER" && password === "USER")) {
       alert("Login successful!");
-      // Example redirect: window.location.href = "/dashboard";
     } else {
       alert("Invalid credentials");
     }
+  }
+
+  @HostListener('document:keydown.enter')
+  handleEnter() {
+    this.validateLogin();
   }
 }
