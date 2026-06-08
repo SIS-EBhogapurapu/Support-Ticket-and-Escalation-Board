685629f37a2166a14be706000aa336012ec7f120
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-08 18:55:42 +0530

    integration of sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, NgModule, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login';
 import { FormsModule } from "@angular/forms";
+import { Sidebar } from './sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], 
-  template: `<router-outlet></router-outlet>`
+  imports: [RouterOutlet,Sidebar], 
+  templateUrl: './app.html'
 })
 export class AppComponent {}
