d4ed031eb987f379d7c38e32ab666dac1dc0a046
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-09 17:43:52 +0530

    user tickets queue component

---
+++ src/app/users-tq/users-tq.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-tq',
+  imports: [],
+  templateUrl: './users-tq.html',
+  styleUrl: './users-tq.css',
+})
+export class UsersTq {}
