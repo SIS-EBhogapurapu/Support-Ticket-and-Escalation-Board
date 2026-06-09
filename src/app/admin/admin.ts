faefc48a42d5f8c8f60d88be33321e421dc5d798
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-09 15:29:36 +0530

    integration of sidebar

--- src/app/admin/admin.ts
+++ src/app/admin/admin.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-admin',
-  standalone:true,
+  standalone: true,
+  imports: [RouterModule, Sidebar],
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-users',
   standalone: true,
+  imports: [RouterModule, Sidebar],  //
   templateUrl: './users.html',
   styleUrls: ['./users.css']
 })
-export class UsersComponent {
-
-}
+export class UsersComponent {}
