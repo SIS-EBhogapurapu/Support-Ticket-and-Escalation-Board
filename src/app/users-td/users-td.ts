f58a20b7bcdfc4f8c02f9095b4518aa6a169e09b
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-10 14:36:41 +0530

    added search bar , filters to ticket queue

--- src/app/users-td/users-td.ts
+++ src/app/users-td/users-td.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { Sidebar } from '../sidebar/sidebar';
 
 @Component({
   selector: 'app-users-td',
-  imports: [],
+  standalone:true,
+  imports: [RouterLink,RouterLinkActive,RouterModule,Sidebar],
   templateUrl: './users-td.html',
   styleUrl: './users-td.css',
 })
-export class UsersTd {}
+export class UsersTdComponent {}
--- src/app/users-tq/users-tq.ts
+++ src/app/users-tq/users-tq.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
 import { Sidebar } from '../sidebar/sidebar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users-tq',
   standalone:true,
-  imports: [RouterModule,RouterLink,RouterLinkActive,Sidebar],
+  imports: [RouterModule,RouterLink,RouterLinkActive,Sidebar,FormsModule],
   templateUrl: './users-tq.html',
   styleUrl: './users-tq.css',
 })
-export class UsersTqComponent {}
+
+export class UsersTqComponent {
+
+  
+  tickets = [
+    { id: 'T001', name: 'Dummy1', priority: 'High', status: 'Open', owner: 'ADMIN' },
+    { id: 'T002', name: 'Dummy2', priority: 'Moderate', status: 'Closed', owner: 'ADMIN' },
+    { id: 'T003', name: 'Dummy3', priority: 'Moderate', status: 'Open', owner: 'ADMIN' },
+    { id: 'T004', name: 'Dummy4', priority: 'Low', status: 'Closed', owner: 'ADMIN' },
+    { id: 'T005', name: 'Dummy5', priority: 'High', status: 'Open', owner: 'ADMIN' }
+  ];
+
+  
+  filteredTickets = [...this.tickets];
+
+  
+  searchText: string = '';
+  selectedPriority: string = '';
+  selectedStatus: string = '';
+
+  
+  applyFilters() {
+    this.filteredTickets = this.tickets.filter(ticket => {
+
+      const matchesSearch =
+        ticket.name.toLowerCase().includes(this.searchText.toLowerCase());
+
+      const matchesPriority =
+        this.selectedPriority === '' ||
+        ticket.priority === this.selectedPriority;
+
+      const matchesStatus =
+        this.selectedStatus === '' ||
+        ticket.status === this.selectedStatus;
+
+      
+      return matchesSearch && matchesPriority && matchesStatus;
+    });
+  }
+}
+
