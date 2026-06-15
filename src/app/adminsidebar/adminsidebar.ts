0fd4d92bf918a8d09f61aedea78cc603a9726c06
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-15 11:06:54 +0530

    set all the buttons in order

--- src/app/adminsidebar/adminsidebar.ts
+++ src/app/adminsidebar/adminsidebar.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-adminsidebar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,RouterModule],
   templateUrl: './adminsidebar.html',
   styleUrls: ['./adminsidebar.css']
 })
