635b0d45af3ae756e1c22ec788abd14b0349fd26
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-15 10:15:30 +0530

    all Required components created for admin screens

---
+++ src/app/admin-newt/admin-newt.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-newt',
+  imports: [],
+  templateUrl: './admin-newt.html',
+  styleUrl: './admin-newt.css',
+})
+export class AdminNewt {}
---
+++ src/app/admin-tdes/admin-tdes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-tdes',
+  imports: [],
+  templateUrl: './admin-tdes.html',
+  styleUrl: './admin-tdes.css',
+})
+export class AdminTdes {}
