1c71a2bec70a2e80e88ff8efb267b4bb2be3bf1b
Eswar Lalith <ebhogapurapu@sisfirst.com>
2026-06-15 10:09:04 +0530

    Admin Tickets queue page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,8 @@ import { AdminComponent } from "./admin/admin";
 import { UsersTqComponent } from "./users-tq/users-tq";
 import { UsersNewtComponent } from './users-newt/users-newt';
 import { UsersTdesComponent } from "./users-tdes/users-tdes";
+import { AdminTqComponent } from './admin-tq/admin-tq';
+
 
 
 export const routes: Routes = [
@@ -14,7 +16,8 @@ export const routes: Routes = [
   { path: 'admin', component: AdminComponent},
   { path: 'Userstq', component: UsersTqComponent},
   { path: 'users-newt', component:UsersNewtComponent}, 
-  { path: 'users-tdes', component: UsersTdesComponent}
+  { path: 'users-tdes', component: UsersTdesComponent},
+  { path: 'admin-tq', component: AdminTqComponent}
 ];
 
 @NgModule({
